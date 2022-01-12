573764aac0f50723055c843d0fa54505c1c8c702
tienhgm <47660593+tienhgm@users.noreply.github.com>
2022-01-12 20:02:40 +0700

    init client

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+import React from 'react';
+import './App.less';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>123</Button>
+    </div>
+  );
+}
+
+export default App;
